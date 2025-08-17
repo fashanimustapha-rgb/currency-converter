3c37aca38ee30b7d52098e08ba1f1de9835ce197
fashanimustapha-rgb <fashanimustapha@gmail.com>
2025-08-17 20:29:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,161 @@ const convertButton = document.getElementById('convert_button');
 const amountInput = document.getElementById('amount');
 const resultSpan = document.querySelector('#result span');
 const swapButton = document.getElementById('swap_button');
-const favoriteButtons = document.querySelectorAll('.favorite-currency'); // متغير جديد لأزرار العملات المفضلة
+const favoriteButtons = document.querySelectorAll('.favorite-currency');
+
+// قائمة بأسماء الدول والعملات
+const countries = {
+    "USD": "الولايات المتحدة الأمريكية",
+    "EUR": "الاتحاد الأوروبي",
+    "MAD": "المغرب",
+    "SAR": "المملكة العربية السعودية",
+    "AED": "الإمارات العربية المتحدة",
+    "KWD": "الكويت",
+    "BHD": "البحرين",
+    "QAR": "قطر",
+    "OMR": "سلطنة عُمان",
+    "JOD": "الأردن",
+    "EGP": "مصر",
+    "DZD": "الجزائر",
+    "TND": "تونس",
+    "GBP": "المملكة المتحدة",
+    "JPY": "اليابان",
+    "CAD": "كندا",
+    "AUD": "أستراليا",
+    "CHF": "سويسرا",
+    "CNY": "الصين",
+    "INR": "الهند",
+    "RUB": "روسيا",
+    "TRY": "تركيا",
+    "SEK": "السويد",
+    "NOK": "النرويج",
+    "DKK": "الدنمارك",
+    "AFN": "أفغانستان",
+    "ALL": "ألبانيا",
+    "AMD": "أرمينيا",
+    "ANG": "جزر الأنتيل الهولندية",
+    "AOA": "أنغولا",
+    "ARS": "الأرجنتين",
+    "AWG": "أروبا",
+    "AZN": "أذربيجان",
+    "BAM": "البوسنة والهرسك",
+    "BBD": "باربادوس",
+    "BDT": "بنغلاديش",
+    "BGN": "بلغاريا",
+    "BIF": "بوروندي",
+    "BMD": "برمودا",
+    "BND": "بروناي",
+    "BOB": "بوليفيا",
+    "BRL": "البرازيل",
+    "BSD": "جزر البهاما",
+    "BTN": "بوتان",
+    "BWP": "بوتسوانا",
+    "BYN": "بيلاروسيا",
+    "BZD": "بليز",
+    "CDF": "الكونغو الديمقراطية",
+    "CLP": "تشيلي",
+    "COP": "كولومبيا",
+    "CRC": "كوستاريكا",
+    "CUP": "كوبا",
+    "CVE": "الرأس الأخضر",
+    "CZK": "جمهورية التشيك",
+    "DJF": "جيبوتي",
+    "DOP": "جمهورية الدومينيكان",
+    "ETB": "إثيوبيا",
+    "FJD": "فيجي",
+    "FKP": "جزر فوكلاند",
+    "GEL": "جورجيا",
+    "GHS": "غانا",
+    "GIP": "جبل طارق",
+    "GMD": "غامبيا",
+    "GNF": "غينيا",
+    "GTQ": "غواتيمالا",
+    "GYD": "غيانا",
+    "HKD": "هونغ كونغ",
+    "HNL": "هندوراس",
+    "HRK": "كرواتيا",
+    "HTG": "هايتي",
+    "HUF": "المجر",
+    "IDR": "إندونيسيا",
+    "ILS": "إسرائيل",
+    "ISK": "آيسلندا",
+    "JMD": "جامايكا",
+    "KES": "كينيا",
+    "KGS": "قيرغيزستان",
+    "KHR": "كمبوديا",
+    "KMF": "جزر القمر",
+    "KRW": "كوريا الجنوبية",
+    "KZT": "كازاخستان",
+    "LAK": "لاوس",
+    "LBP": "لبنان",
+    "LKR": "سريلانكا",
+    "LRD": "ليبيريا",
+    "LSL": "ليسوتو",
+    "LYD": "ليبيا",
+    "MDL": "مولدوفا",
+    "MGA": "مدغشقر",
+    "MKD": "مقدونيا",
+    "MMK": "ميانمار",
+    "MNT": "منغوليا",
+    "MOP": "ماكاو",
+    "MRO": "موريتانيا",
+    "MUR": "موريشيوس",
+    "MVR": "المالديف",
+    "MWK": "مالاوي",
+    "MXN": "المكسيك",
+    "MYR": "ماليزيا",
+    "MZN": "موزمبيق",
+    "NAD": "ناميبيا",
+    "NGN": "نيجيريا",
+    "NIO": "نيكاراغوا",
+    "NPR": "نيبال",
+    "NZD": "نيوزيلندا",
+    "PAB": "بنما",
+    "PEN": "بيرو",
+    "PGK": "بابوا غينيا الجديدة",
+    "PHP": "الفلبين",
+    "PKR": "باكستان",
+    "PLN": "بولندا",
+    "PYG": "باراغواي",
+    "RON": "رومانيا",
+    "RSD": "صربيا",
+    "RWF": "رواندا",
+    "SBD": "جزر سليمان",
+    "SCR": "سيشل",
+    "SDG": "السودان",
+    "SGD": "سنغافورة",
+    "SHP": "سانت هيلانة",
+    "SLL": "سيراليون",
+    "SOS": "الصومال",
+    "SRD": "سورينام",
+    "SSP": "جنوب السودان",
+    "STD": "ساو تومي وبرينسيب",
+    "SVC": "السلفادور",
+    "SYP": "سوريا",
+    "SZL": "سوازيلاند",
+    "THB": "تايلاند",
+    "TJS": "طاجيكستان",
+    "TMT": "تركمانستان",
+    "TOP": "تونغا",
+    "TTD": "ترينيداد وتوباغو",
+    "TWD": "تايوان",
+    "TZS": "تنزانيا",
+    "UAH": "أوكرانيا",
+    "UGX": "أوغندا",
+    "UYU": "أوروغواي",
+    "UZS": "أوزبكستان",
+    "VES": "فنزويلا",
+    "VND": "فيتنام",
+    "VUV": "فانواتو",
+    "WST": "ساموا",
+    "XAF": "فرنك وسط أفريقيا",
+    "XCD": "دولار شرق الكاريبي",
+    "XOF": "فرنك غرب أفريقيا",
+    "XPF": "فرنك المحيط الهادئ",
+    "YER": "اليمن",
+    "ZAR": "جنوب أفريقيا",
+    "ZMW": "زامبيا"
+};
 
 const API_KEY = 'f03b0ea4644e7185abb6dab4';
 const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
@@ -16,15 +170,23 @@ async function fetchCurrencies() {
         const data = await response.json();
         const currencies = Object.keys(data.conversion_rates);
 
+        currencies.sort((a, b) => {
+            const countryA = countries[a] || '';
+            const countryB = countries[b] || '';
+            return countryA.localeCompare(countryB, 'ar', { sensitivity: 'base' });
+        });
+
         currencies.forEach(currency => {
+            const optionText = countries[currency] ? `${countries[currency]} (${currency})` : currency;
+
             const option1 = document.createElement('option');
             option1.value = currency;
-            option1.textContent = currency;
+            option1.textContent = optionText;
             fromCurrency.appendChild(option1);
 
             const option2 = document.createElement('option');
             option2.value = currency;
-            option2.textContent = currency;
+            option2.textContent = optionText;
             toCurrency.appendChild(option2);
         });
 
